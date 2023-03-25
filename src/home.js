fb0405eed61786b02c3d9486e61cd5da7ac0b73c
gaofeng222 <596642721@qq.com>
2023-03-25 09:27:40 +0800

    第一期完成

---
+++ src/home.js
@@ -0,0 +1,4 @@
+import { createRoot } from 'react-dom'
+import App from './App'
+const app = document.getElementById('app')
+createRoot(app).render(<h1>Home</h1>, app)
---
+++ src/profile.js
@@ -0,0 +1,3 @@
+import { createRoot } from 'react-dom'
+const app = document.getElementById('app')
+createRoot(app).render(<h1>Profile</h1>, app)
